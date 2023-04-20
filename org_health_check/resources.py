013beedb6ad9d25dcf73e06cb95b35cbdc6aa8b9
Thomas Camiré <tcamire@coveo.com>
2023-04-20 13:12:12 -0400

    Ricky: org_health_check: Commented out condition from resources while investigating bug in condition check

--- org_health_check/resources.py
+++ org_health_check/resources.py
@@ -3,19 +3,22 @@ import resourcesPrivate
 
 # Dict of resource types
 types = {
-    'ipe'       : resourcesPrivate.CheckIpe      (),
-    'source'    : resourcesPrivate.CheckSource   (),
-    'condition' : resourcesPrivate.CheckCondition(),
-    'qp'        : resourcesPrivate.CheckQp       (),
-    'mlmodel'   : resourcesPrivate.CheckMlModel  (),
-    'field'     : resourcesPrivate.CheckField    ()
+    'ipe': resourcesPrivate.CheckIpe(),
+    'source': resourcesPrivate.CheckSource(),
+    # 'condition' : resourcesPrivate.CheckCondition(), # Commented out while investigating bug in Condition check
+    'qp': resourcesPrivate.CheckQp(),
+    'mlmodel': resourcesPrivate.CheckMlModel(),
+    'field': resourcesPrivate.CheckField()
 }
+
 
 def runsQueries(rKey):
     return types[rKey].runsQueries
 
+
 def needsViewAllContent(rKey):
     return types[rKey].needsViewAllContent
+
 
 def check(rKey):
     return types[rKey].check(rKey)
